0cc677836ad6227f2cd7984c9f17999f609b5b76
Unknown <nicolaspiccitto@gmail.com>
2018-05-23 17:45:30 -0300

    Clase23/05
    Animaciones(Maquinas de estado)

---
+++ Prueba/Assets/Scripts/TestAnimation.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestAnimation : MonoBehaviour {
+    private Rigidbody rb;
+    private Animator anim;
+    [SerializeField] float maxSpeed;
+
+
+
+    private void Awake()
+    {
+        anim = GetComponent<Animator>();
+    }
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+
+   
+    void FixedUpdate()
+    {
+        rb.velocity = transform.forward * Input.GetAxis(" Vertical") * maxSpeed;
+        
+        
+        
+
+            
+    }
+}
