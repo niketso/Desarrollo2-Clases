bb3216e17d80e24e9e9954ccdf4398bafb5ae2a3
Unknown <nicolaspiccitto@gmail.com>
2018-06-13 16:17:08 -0300

    Clase13/06
    Raycast

---
+++ Prueba/Assets/Scripts/Ray.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ray : MonoBehaviour {
+
+    [SerializeField] float range;
+    [SerializeField] GameObject ps;
+
+	void Update () {
+        Debug.DrawRay(transform.position, transform.forward * range,Color.green);
+        RaycastHit hitInfo;
+        if(Input.GetButtonDown ("Fire1"))
+        {
+            if (Physics.Raycast(transform.position, transform.forward, out hitInfo, range))
+               {
+                    Instantiate(ps, hitInfo.point, Quaternion.LookRotation(hitInfo.normal));
+               }
+
+        }
+            
+
+	}
+}
