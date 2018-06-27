5b2e342f9e694f242de82dee8b17da7ab65efd89
Unknown <nicolaspiccitto@gmail.com>
2018-06-27 17:47:05 -0300

    Clase 27/06
    NavMesh

---
+++ Prueba/Assets/navEjemplo.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class navEjemplo : MonoBehaviour {
+
+    [SerializeField] Transform target;
+    [SerializeField] Transform ori;
+    private NavMeshAgent navEje;
+
+
+    private void Awake()
+    {
+        navEje = GetComponent<NavMeshAgent>();
+        navEje.SetDestination(target.position);
+    }
+    private void Update()
+    {
+        if (!navEje.pathPending && navEje.remainingDistance < navEje.stoppingDistance)
+        {
+
+
+        }
+    }
+}
